f7de271fdfb49979ab4b3c2710a29522b1d36c58
Lorenzo Poggi <lorenzo.poggijanin@gmail.com>
2025-04-13 00:37:31 -0300

    muchos cambios juntos

--- Pagina Web/assets/js/main.js
+++ Pagina Web/assets/js/main.js
@@ -63,13 +63,21 @@
   /**
    * Preloader
    */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove();
-    });
-  }
+  window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    const mainContent = document.getElementById('main-content');
 
+    setTimeout(() => {
+      preloader.style.transition = 'opacity 1s ease';
+      preloader.style.opacity = '0';
+
+      setTimeout(() => {
+        preloader.style.display = 'none';
+        mainContent.style.display = 'block';
+      }, 1000);
+
+    }, 2000);
+  });
   /**
    * Scroll top button
    */
